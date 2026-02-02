8e586517cf7ab66af6f4c5e1dcf7c518999ed7cc
joker20020 <405997563@qq.com>
2026-02-02 21:45:58 +0800

    clear repo

--- src/preload/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { ElectronAPI } from '@electron-toolkit/preload'
-import { EntityManagerAPI } from '../shared/types/api'
-
-declare global {
-  interface Window {
-    electron: ElectronAPI
-    api: EntityManagerAPI
-  }
-}
